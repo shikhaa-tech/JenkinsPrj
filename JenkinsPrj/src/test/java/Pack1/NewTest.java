8d4d889f461e15844a118254decd9bd518a945c8
shikhaa-tech <shikhaa.amol03@gmail.com>
2020-09-06 12:45:54 +0100

    commit

---
+++ JenkinsPrj/src/test/java/Pack1/NewTest.java
@@ -0,0 +1,23 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void prg()
+	{
+				
+				System.out.println("Browser opening....");
+				System.setProperty("webdriver.gecko.driver", "G:/geckodriver-v0.26.0-win64/geckodriver.exe");
+				WebDriver w = new FirefoxDriver();
+				w.get("https://www.google.com/");
+				System.out.println("Google opened.....");
+				w.quit();
+				System.out.println("browser closed.....");
+	}
+
+}
